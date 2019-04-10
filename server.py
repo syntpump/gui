0b689947255a50ead64e8ea0beeaf0883d0c83e0
HardCore <43773526+TheHardCo@users.noreply.github.com>
2019-04-10 22:55:09 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request,jsonify
 
 app = Flask(__name__)
 
@@ -8,8 +9,13 @@ def desk():
     return render_template("index.html")
 
 
+@app.route("/word/recognize/")
+def req():
+    data = request.args.get('word', {}, type=str)
+    return jsonify(dict(results=str(data)))
+
 # def ask(word):#
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
