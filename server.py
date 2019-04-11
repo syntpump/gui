16ec54dc2c69c1c18461c6eeb75ce1990d6135ac
Paul Lynn <lynnporu@gmail.com>
2019-04-11 20:12:35 +0300

    Import flask before using, add methods for scipts and styles

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/")
 def desk():
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 
-# def ask(word):#
+@app.route("/js/<string:script>")
+def get_script(script):
+    return flask.send_file(
+        "js/" + script,
+        mimetype="application/javascript"
+    )
+
+
+@app.route("/css/<string:style>")
+def get_style(style):
+    return flask.send_file(
+        "css/" + style,
+        mimetype="text/css"
+    )
 
 
 if __name__ == "__main__":
