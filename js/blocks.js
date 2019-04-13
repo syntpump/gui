0ef09caccb735035bd7468bd7c50c15b8bd5f68f
Paul Lynn <lynnporu@gmail.com>
2019-04-13 21:46:02 +0300

    Add template and js for rule blocks

--- js/blocks.js
+++ js/blocks.js
@@ -3,9 +3,30 @@ class Blocks{
 	*/
 
 	static ruleContainer(args){
+		if(!args["_id"])
+			args["_id"] = "(немає id)";
+
+		let properties = "";
+
+		for(const [key, value] of Object.entries(args)){
+			if(key in Localization.udProps){
+				properties += `
+					<tr>
+						<td>${Localization.udProps[key]}</td>
+						<td>${Localization.udValues[value]}</td>
+					</tr>
+				`;
+			}
+		}
+
 		return `
 			<div class="ruleBlock">
-				<span class="pos">${args["upos"]}</span>
+				<div class="top">
+					<span class="upos">${args["upos"]}</span>
+					<span class="xpos">${args["xpos"]}</span>
+				</div>
+				<table class="properties">${properties}</table>
+				<span class="ruleId">${args["_id"]}</span>
 			</div>
 		`;
 	}
---
+++ js/localization.js
@@ -0,0 +1,13 @@
+Localization = {
+	/*This array contains all variables which should be translated to
+	Ukrainian before showing.
+	*/
+
+	noId: "(немає id)",
+	udProps : {
+		"Tense": "Час"
+	},
+	udValues: {
+		"Pres": "Теперішній"
+	}
+}
