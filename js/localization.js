66289edc5fb5dc16d277fde27c992ab21da8cf23
HardCore <rmuzyka77@gmail.com>
2019-04-17 22:53:41 +0300

    Basic localization variables and their values
    Not finished yet. Need more Ukrainian terminology

--- js/localization.js
+++ js/localization.js
@@ -7,7 +7,27 @@ Localization = {
 	udProps : {
 		"Tense": "Час",
 		"Number": "Число"
-	},
+		"Abbr": "Абревіатура"	
+		"Animacy": "Істота"
+		"Aspect": "Вид дієслова"
+		"Case": "Відмінок"
+		"Degree": "Ступінь порівняння прикметників"
+		"Foreign": "Запозичене"
+		"Gender": "Рід"
+		"Hyph": "Складене слово"
+		"Mood": "Спосіб"
+		"NameType": ""	
+		"NumType": ""
+		"PartType": ""
+		"Person": ""
+		"Polarity": ""
+		"Poss": ""
+		"PronType": ""
+		"PunctType": ""
+		"Reflex": ''
+		"VerbForm": ""
+		"Voice": ""
+	}
 	udValues: {
 		"Pres": "Теперішній",
 		"Past": "Минулий",
@@ -16,5 +36,200 @@ Localization = {
 		"Plur": "Множина",
 		"Dual": "Двоїна",
 		"Ptan": "Парне"
+		abbreviation	av
+abessive	ab
+ablative	al
+absolute superlative	ao "Найвищий ступінь складена форма"
+absolutive	au
+accusative	ac "Знахідний"
+active	at
+additive	ai
+adessive	ad
+admirative	am
+adverbial participle	ap
+affirmative	af
+allative	aa
+animate	"Жива"
+antipassive	ae
+aorist	as
+article	ar
+aspect	ax
+associative	az
+bantu noun class	bc
+benefactive	be
+cardinal	ca
+case	cs
+causative case	cc
+causative voice	cv
+clusivity	cl
+collective noun	cn
+collective numeral	cu
+collective pronominal	cp
+comitative	cm
+common gender	cg "Спільний"
+comparative case	cy
+comparative degree	cd "Вищий ступінь"
+complex definiteness	cf
+conditional	ci "Умовний"
+conjunctive	cj
+considerative	ce
+construct state	cr
+converb	cb
+count plural	co
+counting form	cx
+dative	dt "Давальний"
+definite	df
+definiteness	ds
+degree of comparison	dc
+delative	dl
+demonstrative	dm
+desiderative	dv
+destinative	dx
+direct case	de
+direct voice	do
+directional allative	di
+distributive case	du
+distributive numeral	dn
+dual	da
+elative	el
+elevated referent	er
+emphatic	eh
+equative case	ec
+equative degree	ed
+ergative	eg
+essive	es
+evidentiality	ev
+exclamative	ex
+exclusive	eu
+factive	fa
+feminine	fm "Жіночий"
+finite verb	fv
+first person	1p
+firsthand	fh
+foreign word	fw
+formal	fo
+fourth person	4p
+fraction	fq
+frequentative	fu
+future	ft
+gender	ge
+genitive	gn "Родовий"
+gerund	gr
+gerundive	gv
+greater paucal	gp
+greater plural	gl
+habitual	ha "Теперішній простий"
+human	hu "Людська"
+humbled speaker	hs
+illative	il
+imperative	im "Наказовий"
+imperfect tense	it
+imperfective aspect	ia "Інфінітив"
+inanimate	in "Нежива"
+inclusive	ic
+indefinite	id
+indefinite pronominal	ip
+indicative	ii "Дійсний"
+inessive	ie
+infinitive	if
+informal	io
+injunctive	ij
+instructive	is
+instrumental	ix "Орудний"
+interrogative	ir
+inverse number	iu
+inverse voice	iv
+iterative	iy "Повторювана дія"
+jussive	ju
+lative	la
+locative	lo "Місцевий"
+masculine	ma "Чоловіча"
+masdar	ms
+mass noun	mn
+middle voice	mv
+modality	md
+mood	mo
+motivative	mt
+multiplicative numeral	ml
+narrative	na
+necessitative	ne
+negative polarity	np
+negative pronominal	nr
+neuter	nu "Середній"
+nominative	no "Називний"
+non-finite verb	nv
+non-firsthand	nf
+non-human	nh "Не людська"
+non-specific indefinite	ni
+noun class	nc
+number	nm
+numeral type	nt
+oblique case	oq
+optative	op
+ordinal	or
+participle	pa
+partitive	pv
+passive	p1
+past	pt
+past perfect	pp
+paucal	pu
+perfective aspect	pf "Доконаний"
+perlative	pr
+person	po
+personal	pl
+pluperfect	pe
+plural	pb
+plurale tantum	pc
+polarity	pd
+politeness	pn
+positive degree	pg "Якісний прикметник"
+positive polarity	pw
+possessive	ps
+potential	pi
+present	ph
+preterite	pj
+progressive	pk "Недоконаний"
+prolative	px
+pronominal type	py
+prospective	pz
+purposive case	p0
+purposive mood	pq
+quantifier	qf
+quantitative plural	qp
+quotative	qt
+range numeral	rn
+reciprocal pronominal	rp
+reciprocal voice	rv
+reduced definiteness	rd
+reflexive	rf
+register	rg
+relative	rl
+second person	2p
+set numeral	sn
+singular	si
+singulare tantum	st
+specific indefinite	sf
+subjunctive	sj
+sublative	sb
+superessive	sp
+superlative	sl "Найвищий ступінь"
+supine	su
+temporal	tm
+tense	te
+terminal allative	ta
+terminative	tr
+third person	3p
+total	to
+transgressive	tg
+translative	ts
+trial	ti
+uter	ut
+verb form	vf
+verbal adjective	va
+verbal adverb	vv
+verbal noun	vn
+vocative	vo "Кличний"
+voice	vi
+zero person
 	}
 }
