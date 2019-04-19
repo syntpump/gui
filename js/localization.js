ed24db22721437a0293b542b71f5cb78f3586140
Paul Lynn <lynnporu@gmail.com>
2019-04-19 13:09:52 +0300

    Add commas, delete spaces

--- js/localization.js
+++ js/localization.js
@@ -6,28 +6,28 @@ Localization = {
 	noId: "(немає id)",
 	udProps : {
 		"Tense": "Час",
-		"Number": "Число"
-		"Abbr": "Абревіатура"	
-		"Animacy": "Істота"
-		"Aspect": "Вид дієслова"
-		"Case": "Відмінок"
-		"Degree": "Ступінь порівняння"
-		"Foreign": "Запозичене"
-		"Gender": "Рід"
-		"Hyph": "Складене слово"
-		"Mood": "Спосіб"
-		"NameType": "Власна назва"	
-		"NumType": "Тип числа"
-		"PartType": "Вид частки"
-		"Person": "Особа"
-		"Polarity": "Ствердність"
-		"Poss": "Присвійність"
-		"PronType": "Тип "
-		"PunctType": "Займенниковий тип"
-		"Reflex": "Рефлективність"
-		"VerbForm": "Дієслівна форма"
+		"Number": "Число",
+		"Abbr": "Абревіатура",
+		"Animacy": "Істота",
+		"Aspect": "Вид дієслова",
+		"Case": "Відмінок",
+		"Degree": "Ступінь порівняння",
+		"Foreign": "Запозичене",
+		"Gender": "Рід",
+		"Hyph": "Складене слово",
+		"Mood": "Спосіб",
+		"NameType": "Власна назва",
+		"NumType": "Тип числа",
+		"PartType": "Вид частки",
+		"Person": "Особа",
+		"Polarity": "Ствердність",
+		"Poss": "Присвійність",
+		"PronType": "Тип ",
+		"PunctType": "Займенниковий тип",
+		"Reflex": "Рефлективність",
+		"VerbForm": "Дієслівна форма",
 		"Voice": "Стан"
-	}
+	},
 	udValues: {
 		"pres": "Теперішній",
 		"past": "Минулий",
@@ -35,60 +35,60 @@ Localization = {
 		"sing": "Однина",
 		"plur": "Множина",
 		"dual": "Двоїна",
-		"ptan": "Парне"
-		"absolute superlative": "Найвищий складений"
-		"accusative": "Знахідний"
-		"active": "Дійсний"
-		"animate": "Жива"
-		"cardinal": "Прості"
-		"common gender": "Спільний"
-		"comparative degree": "Вищий"
-		"conditional": "Умовний"
-		"conseq": "Пояснювальна"
-		"dative": "Давальний"
-		"demonstrative": "Вказівний"
-		"feminine": "Жіночий"
-		"finite verb": "Способове"
-		"first person": "Перша особа"
-		"fraction": "Дробові"
-		"geographical name": "Географічна"
-		"genitive": "Родовий"
-		"habitual": "Теперішній простий"
-		"human": "Людська"
-		"imperative": "Наказовий"
-		"imperfective aspect": "Інфінітив"
-		"inanimate": "Нежива"
-		"indefinite pronominal": "Невизначений"
-		"indicative": "Дійсний"
-		"infinitive": "Інфінітив"
-		"instrumental": "Орудний"
-		"interrogative": "Питальний"
-		"iterative": "Повторювана дія"
-		"locative": "Місцевий"
-		"masculine": "Чоловіча"
-		"multiplicative numeral": "Множинні"
-		"negative pronominal": "Заперечний"
-		"nationality": "Національність"
-		"neuter": "Середній"
-		"nominative": "Називний"
-		"non-human": "Не людська"
-		"optative": "Бажальний"
-		"ordinal": "Порядкові"
-		"participle": "Дієприслівник"
-		"passive": "Пасивний"
-		"perfective aspect": "Доконаний"
-		"person": "Ім’я людини"
-		"personal": "Присвійний"
-		"positive degree": "Якісний"
-		"progressive": "Недоконаний"
-		"purposive mood": "Спонукальний"
-		"relative": "Відносний"
-		"second person": "Друга особа"
-		"set numeral": "Для незлічуваних"
-		"surname": "Прізвище"
-		"superlative": "Найвищий"
-		"third person": "Третя особа"
-		"total": "Збірний"
+		"ptan": "Парне",
+		"absolute superlative": "Найвищий складений",
+		"accusative": "Знахідний",
+		"active": "Дійсний",
+		"animate": "Жива",
+		"cardinal": "Прості",
+		"common gender": "Спільний",
+		"comparative degree": "Вищий",
+		"conditional": "Умовний",
+		"conseq": "Пояснювальна",
+		"dative": "Давальний",
+		"demonstrative": "Вказівний",
+		"feminine": "Жіночий",
+		"finite verb": "Способове",
+		"first person": "Перша особа",
+		"fraction": "Дробові",
+		"geographical name": "Географічна",
+		"genitive": "Родовий",
+		"habitual": "Теперішній простий",
+		"human": "Людська",
+		"imperative": "Наказовий",
+		"imperfective aspect": "Інфінітив",
+		"inanimate": "Нежива",
+		"indefinite pronominal": "Невизначений",
+		"indicative": "Дійсний",
+		"infinitive": "Інфінітив",
+		"instrumental": "Орудний",
+		"interrogative": "Питальний",
+		"iterative": "Повторювана дія",
+		"locative": "Місцевий",
+		"masculine": "Чоловіча",
+		"multiplicative numeral": "Множинні",
+		"negative pronominal": "Заперечний",
+		"nationality": "Національність",
+		"neuter": "Середній",
+		"nominative": "Називний",
+		"non-human": "Не людська",
+		"optative": "Бажальний",
+		"ordinal": "Порядкові",
+		"participle": "Дієприслівник",
+		"passive": "Пасивний",
+		"perfective aspect": "Доконаний",
+		"person": "Ім’я людини",
+		"personal": "Присвійний",
+		"positive degree": "Якісний",
+		"progressive": "Недоконаний",
+		"purposive mood": "Спонукальний",
+		"relative": "Відносний",
+		"second person": "Друга особа",
+		"set numeral": "Для незлічуваних",
+		"surname": "Прізвище",
+		"superlative": "Найвищий",
+		"third person": "Третя особа",
+		"total": "Збірний",
 		"vocative": "Кличний"
 	}
 }
